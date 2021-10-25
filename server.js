10e734b01a56a718bd0019b1d59aa69edc6fe188
Dave Quinn <dquinn8@cogeco.ca>
2021-10-25 18:40:08 -0400

    added fix for cors issues

--- server.js
+++ server.js
@@ -13,6 +13,23 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested, Content-Type, Accept Authorization"
+    )
+    if (req.method === "OPTIONS") {
+        res.header(
+            "Access-Control-Allow-Methods",
+            "POST, PUT, PATCH, GET, DELETE"
+        )
+        return res.status(200).json({})
+    }
+    next()
+});
+
+
 //import animals data
 const { animals } = require('./data/animals');
 
